5e6c9cbedd6fa3fd3359332e68fb9d75ba4f8d4b
visalvani <vs.visalakshi@gmail.com>
2019-03-15 11:08:52 +0530

    path message

---
+++ src/sample.java
@@ -0,0 +1,17 @@
+
+public class sample {
+
+	public static void main(String[] args) {
+	int num[]=new int[5];
+	num[0]=1;
+	num[1]=2;			
+	num[3]=4;
+	num[4]=5;
+	num[2]=3;
+	num[0]=10;
+	
+	for(int i=0;i<num.length;i++) {
+	System.out.println(num[i]);
+	}
+	}
+}
